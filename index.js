9e79a9a9741f1e700d32316ae0c39908dd5d8f03
Lansky <luizlanskydev@gmail.com>
2024-04-25 15:29:24 -0300

    add JavaScript to handle fetching data from the API

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const authorName = document.querySelector('#authorName');
-const quote = document.querySelector('#quote');
-const generateBtn = document.querySelector('#generateBtn');
+const authorName = document.querySelector("#authorName");
+const quote = document.querySelector("#quote");
+const generateBtn = document.querySelector("#generateBtn");
+const apiKey = "wzX+JTp+B3AaV1T8in9Mvw==fyNtHJtDFQtZHfLT";
+
+const getData = () => {
+  fetch("https://api.api-ninjas.com/v1/quotes", {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      authorName.textContent = data[0].author;
+      quote.textContent = data[0].quote;
+    });
+};
+
+
+generateBtn.addEventListener("click", ()=> {
+    getData();
+});
